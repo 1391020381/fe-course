b7ab55e2a003dd4069ba53abc36a9e37ec64130c
周进 <zhoujin@zhoujindeMacBook-Pro.local>
2021-07-27 21:56:17 +0800

    showSaveFilePicker与FileReader

--- homework/js-basics/src/views/monaco-editor.vue
+++ homework/js-basics/src/views/monaco-editor.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="content" id="monaco-editor"></div>
+  <div class="content">
+    <div id="monaco-editor"></div>
+    <div class="get-file"><input type="file" @change="showFile" /></div>
+  </div>
 </template>
 
 <script>
@@ -7,7 +10,21 @@ import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import "monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution";
 export default {
   data() {
-    return {};
+    return {
+      filePickerOptions: {
+        types: [
+          {
+            description: "any",
+            accept: {
+              // "image/*": [".png", ".gif", ".jpeg", ".jpg"],
+            },
+          },
+        ],
+        excludeAcceptAllOption: false,
+        multiple: false,
+      },
+      monacoEditor: null,
+    };
   },
   created() {
     this.$nextTick(() => {
@@ -16,11 +33,65 @@ export default {
   },
   methods: {
     createMonacoEditor() {
-      monaco.editor.create(document.getElementById("monaco-editor"), {
-        value: `const foo = () => 0;`,
-        language: "javascript",
-        theme: "vs-dark",
-      });
+      let that = this;
+      this.monacoEditor = monaco.editor.create(
+        document.getElementById("monaco-editor"),
+        {
+          value: "",
+          language: "javascript",
+          fontSize: 14,
+          autoIndent: true,
+          automaticLayout: true,
+          theme: "vs-dark",
+        }
+      );
+      this.monacoEditor.executeEdits("", [
+        {
+          range: {
+            startLineNumber: this.monacoEditor.getPosition().lineNumber,
+            startColumn: this.monacoEditor.getPosition().column,
+            endLineNumber: this.monacoEditor.getPosition().lineNumber,
+            endColumn: this.monacoEditor.getPosition().column,
+          },
+          forceMoveMarkers: true,
+        },
+      ]);
+      this.monacoEditor.addCommand(
+        monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S,
+        function () {
+          console.log("SAVE pressed!");
+          let value = that.monacoEditor.getValue();
+          that.saveFile(value);
+          console.log(value, "-------");
+        }
+      );
+    },
+    showFile(e) {
+      let files = e.target.files;
+      console.log(files);
+      let file = files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        console.log(e.target.result);
+        this.monacoEditor.setValue(e.target.result);
+        return e.target.result;
+      };
+      reader.readAsText(file);
+    },
+    async saveFile(contents) {
+      let file = "";
+      try {
+        file = await window.showSaveFilePicker(this.filePickerOptions);
+      } catch (e) {
+        if (e.message.indexOf("The user aborted a request.") === -1) {
+          console.error(e);
+        }
+        return;
+      }
+      // 然后接前面的代码
+      const writable = await file.createWritable();
+      await writable.write(contents);
+      await writable.close();
     },
   },
 };
@@ -30,5 +101,12 @@ export default {
 .content {
   width: 1200px;
   height: 400px;
+  #monaco-editor {
+    width: 100%;
+    height: 500px;
+  }
+  .get-file {
+    margin-top: 50px;
+  }
 }
 </style>
