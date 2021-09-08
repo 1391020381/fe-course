9fb4371527747f270f149cdb3968b9c4f28cdb42
1391020381 <1391020381@qq.com>
2021-09-08 21:08:50 +0800

    week09

---
+++ homework/week09/src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="24">
+        <div class="app-header">Form-Render</div>
+      </el-col>
+    </el-row>
+    <el-row class="app-main">
+      <el-col :span="3">
+        <app-left
+          @com-dragend="onDragend"
+          @com-select="(name)=>{$refs.middle.addComponent(name)}"
+        ></app-left>
+      </el-col>
+      <el-col :span="16">
+        <app-middle
+          ref="middle"
+          @show-props="(item, elementItem) => {$refs.right.setItem(item, elementItem)}"
+        ></app-middle>
+      </el-col>
+      <el-col :span="5">
+        <app-right
+          ref="right"
+          @change="(key, value) => { $refs.middle.changeProps(key, value) }"
+          @item-change="(key, value) => { $refs.middle.changeItem(key, value) }"
+        ></app-right>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import AppLeft from "./components/left.vue";
+import AppRight from "./components/right.vue";
+import AppMiddle from "./components/middle.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppLeft,
+    AppRight,
+    AppMiddle
+  },
+  methods: {
+    onDragend() {
+      this.$refs.middle.setDragging(false);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+body,
+html {
+  height: 100%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+}
+.app-header {
+  height: 60px;
+  line-height: 60px;
+  padding-left: 30px;
+  box-shadow: 0 8px 24px -2px rgba(0, 0, 0, 0.05);
+  border-bottom: 1px solid #d9d9d9;
+  font-weight: bold;
+  font-size: 20px;
+}
+.app-main {
+  height: calc(100% - 80px);
+  & > * {
+    height: 100%;
+  }
+}
+</style>
---
+++ homework/week09/src/components/renderComponents.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-form
+    ref="ruleForm"
+    label-width="100px"
+    class="demo-ruleForm"
+    :model="model"
+  >
+    <el-form-item
+      v-for="(item, index) in componentData"
+      :key="index"
+      :label="item.label"
+    >
+      <component
+        :is="item.component"
+        v-bind="item.propValue"
+        v-model="model[item.$id]"
+      />
+    </el-form-item>
+  </el-form>
+</template>
+<script>
+import ElSelect from "../widgets/select.vue";
+import ElRadio from "../widgets/radio.vue";
+export default {
+  components: {
+    ElSelect,
+    ElRadio
+  },
+  props: {
+    componentData: {
+      type: Array,
+      default: () => []
+    },
+    model: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.demo-ruleForm {
+  flex: 1;
+}
+</style>
+
