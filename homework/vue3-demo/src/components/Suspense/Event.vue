18e756bf5895ad0f83ef0d0250c5b12b7058d11b
zhoujin <171226Q2>
2021-09-24 10:51:16 +0800

    vue3-demo

---
+++ homework/vue3-demo/src/components/Suspense/Event.vue
@@ -0,0 +1,28 @@
+<template></template>
+
+<script>
+import { ref } from "vue";
+export default {
+  async setup() {
+    const count = ref(null);
+    function getEventCount() {
+      return new Promise((resolve, reject) => {
+        if (Math.random() > 0.5) {
+          console.log("error......", Math.random());
+          reject("get Event Error.");
+        } else {
+          setTimeout(() => resolve(3), 1000);
+        }
+      });
+    }
+    try {
+      count.value = await getEventCount();
+    } catch (e) {
+      throw e;
+    }
+  },
+};
+</script>
+
+<style>
+</style>
